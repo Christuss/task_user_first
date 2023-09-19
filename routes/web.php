5b1ddc85302595d39fbf853a11bf57f1c22539f9
Christuss <szendefi.krisztofer@gmail.com>
2023-09-19 11:22:25 +0200

    new, edit blade

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,8 @@ Route::get('/', function () {
 Route::get('/api/task', [TaskController::class, 'index']);
 Route::get('/api/task/{id}', [TaskController::class, 'show']);
 Route::get('/task/list', [TaskController::class, 'listView']);
+Route::get('/task/new', [TaskController::class, 'newView']);
+Route::get('/task/edit/{id}', [TaskController::class, 'editView']);
+
 
 //require __DIR__ . '/auth.php';
